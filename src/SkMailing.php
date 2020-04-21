ac61f92050caa11fd57854b0c2ab45747e4ee8a8
unknown <n.elfoudani@gmail.com>
2020-04-21 11:26:26 +0000

    file added

--- src/SkMailing.php
+++ src/SkMailing.php
@@ -5,57 +5,106 @@ namespace SkMailing;
 class SkMailing
 {
 
+
     //*********************** Send Mail ************************
     public function sendmail($data, $message_id, $response_format = 'json')
     {
-        if ($data != null && $message_id != null){
+        $result['status'] = false;
+        $result['message'] = '';
 
-                $curl_handler = curl_init();
-                curl_setopt($curl_handler, CURLOPT_URL, 'http://localhost/mailing-list/asm/' . $message_id);
-                //curl_setopt($curl_handler, CURLOPT_URL, $this->getSetting('MAIL_SERVICE_URL_SEND_MESSAGE', true) . $message_id);
-                curl_setopt($curl_handler, CURLOPT_VERBOSE, true);
-                curl_setopt($curl_handler, CURLOPT_SSL_VERIFYPEER, false);
-                curl_setopt($curl_handler, CURLOPT_SSL_VERIFYHOST, false);
-                curl_setopt($curl_handler, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($curl_handler, CURLOPT_POST, true);
-                //$data['api_key'] = Configure::read('Global.gooconsole_mailing_api_key');
-                $fields_string = http_build_query($data);
-                curl_setopt($curl_handler, CURLOPT_POSTFIELDS, $fields_string);
-                $http_responder = curl_exec($curl_handler);
-                //debug($http_responder);
-                //$this->log_info($http_responder);
-                curl_close($curl_handler);
-                if($response_format == 'json'){
-                    return $http_responder;
-                }else{
-                    return json_decode($http_responder, true);
-                }      
+        if ($message_id != null && !empty($message_id)) {
+            $result['status'] = true;
+        } else {
+            $result['status'] = false;
+            $result['message'] = 'You must provide a Message id';
+        }
+
+        if ($result['status'] && $data != null && isset($data['email']) && $this->isMailValid($data['email'])) {
+            $result['status'] = true;
+        } else {
+            $result['status'] = false;
+            $result['message'] = 'You must provide a valid Address Mail';
+        }
+
+        if ($result['status']) {
+
+            $curl_handler = curl_init();
+            curl_setopt($curl_handler, CURLOPT_URL, 'http://localhost/mailing-list/asm/' . $message_id);
+            //curl_setopt($curl_handler, CURLOPT_URL, $this->getSetting('MAIL_SERVICE_URL_SEND_MESSAGE', true) . $message_id);
+            curl_setopt($curl_handler, CURLOPT_VERBOSE, true);
+            curl_setopt($curl_handler, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl_handler, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($curl_handler, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl_handler, CURLOPT_POST, true);
+            //$data['api_key'] = Configure::read('Global.gooconsole_mailing_api_key');
+            $fields_string = http_build_query($data);
+            curl_setopt($curl_handler, CURLOPT_POSTFIELDS, $fields_string);
+            $http_responder = curl_exec($curl_handler);
+            //debug($http_responder);
+            //$this->log_info($http_responder);
+            curl_close($curl_handler);
+            $result['result'] = json_decode($http_responder, true);
+        }
+
+        if ($response_format == 'json') {
+            return json_encode ($result);
+        } else {
+            return $result;
         }
     }
-
     //********************* Subscription ************************
     public function subscribe_to_ml($data, $ml_id, $response_format = 'json')
     {
-        if ($data != null && $ml_id != null){
-                //$data = array_merge($data, $this->addDefaultDataToDataToSend());
-                $curl_handler = curl_init();
-                curl_setopt($curl_handler, CURLOPT_URL, 'http://localhost/mailing-list/asm/' . $ml_id);
-                //curl_setopt($curl_handler, CURLOPT_URL, $this->getSetting('MAIL_SERVICE_URL_SUBSCRIBE', true) . $ml_id);
-                curl_setopt($curl_handler, CURLOPT_VERBOSE, true);
-                curl_setopt($curl_handler, CURLOPT_SSL_VERIFYPEER, false);
-                curl_setopt($curl_handler, CURLOPT_SSL_VERIFYHOST, false);
-                curl_setopt($curl_handler, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($curl_handler, CURLOPT_POST, true);
-                //$data['api_key'] = Configure::read('Global.gooconsole_mailing_api_key');
-                $fields_string = http_build_query($data);
-                curl_setopt($curl_handler, CURLOPT_POSTFIELDS, $fields_string);
-                $http_responder = curl_exec($curl_handler);
-                curl_close($curl_handler);
-                if($response_format == 'json'){
-                    return $http_responder;
-                }else{
-                    return json_decode($http_responder, true);
-                }
+        $result['status'] = false;
+        $result['message'] = '';
+
+        if ($ml_id != null && !empty($ml_id)) {
+            $result['status'] = true;
+        } else {
+            $result['status'] = false;
+            $result['message'] = 'You must provide a Message id';
+        }
+
+        if ($result['status'] && $data != null && isset($data['email']) && $this->isMailValid($data['email'])) {
+            $result['status'] = true;
+        } else {
+            $result['status'] = false;
+            $result['message'] = 'You must provide a valid Address Mail';
+        }
+
+        if ($result['status']) {
+            //$data = array_merge($data, $this->addDefaultDataToDataToSend());
+            $curl_handler = curl_init();
+            curl_setopt($curl_handler, CURLOPT_URL, 'http://localhost/mailing-list/asm/' . $ml_id);
+            //curl_setopt($curl_handler, CURLOPT_URL, $this->getSetting('MAIL_SERVICE_URL_SUBSCRIBE', true) . $ml_id);
+            curl_setopt($curl_handler, CURLOPT_VERBOSE, true);
+            curl_setopt($curl_handler, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl_handler, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($curl_handler, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl_handler, CURLOPT_POST, true);
+            //$data['api_key'] = Configure::read('Global.gooconsole_mailing_api_key');
+            $fields_string = http_build_query($data);
+            curl_setopt($curl_handler, CURLOPT_POSTFIELDS, $fields_string);
+            $http_responder = curl_exec($curl_handler);
+            curl_close($curl_handler);
+            $result['result'] = json_decode($http_responder, true);
+        }
+
+        if ($response_format == 'json') {
+            return json_encode ($result);
+        } else {
+            return $result;
+        }
+    }
+
+
+    //************ Mail validation *******************//   
+    public function isMailValid($email)
+    {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
